5add9f49508a5b1a8d5154e02d66945dfdafd282
satyam kumar <krsatyamthakur@gmail.com>
2026-05-22 03:33:58 +0530

    core java

---
+++ springboot/core java/hello.java
@@ -0,0 +1,12 @@
+public class hello{
+    public static void main(String[] args){
+        // Scanner scn=new Scanner(System.in);
+        // int a=34;
+        // int b=343;
+        // System.out.println(a+b);
+
+        byte b=127;
+
+        System.err.println(b);
+    }
+}
---
+++ springboot/core java/satyam.java
@@ -0,0 +1,13 @@
+
+public class satyam {
+    public static void main(String[] args){
+
+        // byte b =  127;
+        // int a = b;
+        // System.out.println(a);
+
+        int a = 257;
+        byte b = (byte) a;
+        System.out.println(b);
+    }
+}
