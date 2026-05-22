2a8bd6b1b8f49789221e3568a6df86c4083e6fb1
satyam kumar <krsatyamthakur@gmail.com>
2026-05-23 02:00:18 +0530

    oops

---
+++ springboot/oops/oops.java
@@ -0,0 +1,18 @@
+class cal{
+    int a;
+
+    public int add(int a,int b){
+        return a*b;
+
+    }
+}
+public class oops{
+    public static void main(String[] args){
+        int a=34;
+        int b=3434;
+        cal c=new cal();
+        c.add(a,b);
+
+        
+    }
+}
