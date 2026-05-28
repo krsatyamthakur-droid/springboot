27034ca34510b6ac14e970935597b8336e38901f
satyam kumar <krsatyamthakur@gmail.com>
2026-05-28 19:03:26 +0530

    naming

---
+++ springboot/naming convension/demo.java
@@ -0,0 +1,21 @@
+public class demo{
+
+    public void show() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    // camel casing nameing convention
+    String firstName = "John";
+    String lastName = "Doe";
+    String fullName = firstName + " " + lastName;
+
+    // class and interface - capital letter
+    // variable and method - camelCase small
+    // constanst - capitals letter  PIE BRAND
+    new demo().show(); // anonymous object 
+
+
+    }
+}
