b14a59eea0324e66849779803a06fa643683e9d2
satyam kumar <krsatyamthakur@gmail.com>
2026-05-30 03:47:20 +0530

    polymorphism

---
+++ springboot/oops/polymorphism /demo.java
@@ -0,0 +1,27 @@
+// polymorphism means many forms
+// like for example, a method can have different implementations based on the object that it is called on.
+// and like you have different personalaties in front of family and friends.
+
+// THERE ARE TWO TYPE OF POLYMORPHISM
+// 1. Compile-time polymorphism (method overloading) EARLY BINDING
+// 2. Runtime polymorphism (method overriding) LATE BINDING
+
+
+// over loading different parameter 
+// class A{
+//     public int add(int a, int b){
+//         return a+b;
+//     }
+//     public double add(double a, double b){
+//         return a+b;
+//     }
+// }
+// over riding
+// class B extends A{
+//     public int add(int a, int b){
+//         return a*b;
+//     }
+//     public double add(double a, double b){
+//         return a*b;
+//     }
+// }
