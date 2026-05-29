c0ed86fed23972e6d7272751a905ee186185cf49
satyam kumar <krsatyamthakur@gmail.com>
2026-05-29 15:14:06 +0530

    method overiding

--- springboot/oops/this and super /mehtos overiding /demo.java
+++ springboot/oops/this and super /mehtos overiding /demo.java
@@ -1,5 +1,37 @@
+class calc{
+    public int add(int a,int b){
+        return a+b;
+
+    }
+}
+class advcalc extends calc{
+    public int add(int a,int b){
+     return a+b+1;
+    }
+}
+// class A{
+//     public void show(){
+//     System.out.println("hello james of A series ");
+//     }
+    // public void config(){
+    //     System.out.println("this is config of A");
+    // }
+// }
+// class B extends advcalc{
+//     public void show (){
+//    System.out.println("this is show in B");
+
+//     }  
+// }
 public class demo{
     public static void main(String[] args) {
-        
+       // B obj=new B();
+       // obj.show();
+       // obj.config();
+       advcalc obj=new advcalc();
+     int result= obj.add(9,4);
+     System.out.println(result);
+
+
     }
     }
