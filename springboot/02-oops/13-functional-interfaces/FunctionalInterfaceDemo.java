24559eb1705762eadf1ad0236f5828053df788cf
satyam kumar <krsatyamthakur@gmail.com>
2026-06-30 09:30:44 +0530

    comit

--- springboot/02-oops/13-functional-interfaces/FunctionalInterfaceDemo.java
+++ springboot/02-oops/13-functional-interfaces/FunctionalInterfaceDemo.java
@@ -1,28 +1,52 @@
-// TYPES OF INTERFACES
-// 1. Marker Interface: an interface with no methods or fields, used to mark classes
-//    (e.g. java.io.Serializable - marks a class as serializable/deserializable
-//    so its objects can be converted to/from a byte stream and saved to disk).
-// 2. Functional Interface: an interface with a single abstract method (SAM),
-//    used for lambda expressions.
-// 3. Tagging Interface: similar to a marker interface, used to provide metadata.
 
-@FunctionalInterface
-interface A {
-    void config(int i, int j);
+// //    (e.g. java.io.Serializable - marks a class as serializable/deserializable
+// //    so its objects can be converted to/from a byte stream and saved to disk).
+// // 2. Functional Interface: an interface with a single abstract method (SAM),
+// //    used for lambda expressions.
+// // 3. Tagging Interface: similar to a marker interface, used to provide metadata.
+
+// @FunctionalInterface
+// interface A {
+//     void config(int i, int j);
+// }
+
+// public class FunctionalInterfaceDemo {
+//     public static void main(String[] args) {
+//         // A functional interface has exactly one abstract method, so it can be
+//         // implemented with a lambda expression instead of an anonymous class:
+//         //
+//         // A obj = new A() {
+//         //     public void config(int i, int j) {
+//         //         System.out.println("Configuring A" + (i + j));
+//         //     }
+//         // };
+
+//         A obj = (i, j) -> System.out.println("Configuring A" + (i + j)); // lambda expression
+//         obj.config(343, 353);
+//     }
+// }
+//@FunctionalInterface
+interface A{
+    int add(int i,int j);
+
 }
 
-public class FunctionalInterfaceDemo {
-    public static void main(String[] args) {
-        // A functional interface has exactly one abstract method, so it can be
-        // implemented with a lambda expression instead of an anonymous class:
-        //
-        // A obj = new A() {
-        //     public void config(int i, int j) {
-        //         System.out.println("Configuring A" + (i + j));
-        //     }
-        // };
+public class FunctionalInterfaceDemo{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    
+    // using lambda expression as simple as that 
+   // A obj2 = (i) -> System.out.println("Hello from c" + i);
+  //  obj2.show(51);
 
-        A obj = (i, j) -> System.out.println("Configuring A" + (i + j)); // lambda expression
-        obj.config(343, 353);
+A obj=(a,b) -> a+b;
+int result=obj.add(435,35);
+System.out.println(result);
     }
-}
+    }
+// exception
+// Error 
+// 1 compile time error 
+// 2 Run time error - exception
+// 3 logical error
+
---
+++ springboot/02-oops/types of interfaces/demo.java
@@ -0,0 +1,16 @@
+public class demo{
+    public static void main(String[] args) {
+     System.out.println("Hello World");
+
+     // types of interfaces
+     // 1. Marker Interface- A marker interface is an interface with no methods or fields. 
+     //It is used to indicate that a class possesses a certain property or behavior.
+     // 2. Functional Interface / sam-single abstract method - 
+     //A functional interface is an interface that contains exactly one abstract method. It can be used as the assignment target for a lambda expression or method reference.
+     // 3. Tagging Interface - two or more interfaces can be combined to form 
+     //a tagging interface, which is used to add metadata to a class.
+     // serialization- Marker interface used in serialization process to indicate that a class can be serialized.
+     // deserialization- Marker interface used in deserialization process to indicate that a class can be deserialized.
+
+    }
+    }
