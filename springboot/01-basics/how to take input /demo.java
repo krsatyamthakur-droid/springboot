2a4145b99199bb3005f85365b5c8b0660548790a
satyam kumar <krsatyamthakur@gmail.com>
2026-07-01 07:43:57 +0530

    inputs

---
+++ springboot/01-basics/how to take input /demo.java
@@ -0,0 +1,21 @@
+public class demo {
+    public static void main(String[] args) {
+    //    Scanner scn=new Scanner(System.in);
+    //    int n=scn.nextInt();
+    //    System.out.println(n);
+      int i=2;
+        int j=0;
+    try {
+       j=18/i;
+       System.out.println("hello james ");
+
+    } catch (Exception e) {
+        System.out.println("Something went wrong");
+    }
+    finally{
+
+    }
+
+    }
+}
+// when you not using catch just use finally 
