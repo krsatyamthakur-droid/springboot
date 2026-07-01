c63375333115118a013944bec5c046dda9fc7cbd
satyam kumar <krsatyamthakur@gmail.com>
2026-07-01 07:25:17 +0530

    throw

---
+++ springboot/02-oops/exceptions/demo.java
@@ -0,0 +1,72 @@
+// public class demo{
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//  // exception
+// // Error 
+// // 1 compile time error 
+// // 2 Run time error - exception
+// // 3 logical error
+
+// int a=34342;
+// int b=10;
+// int i=0;
+// int arr[]=new int[3];
+// arr[2]=23;
+
+// try {
+//     int j=343432/2;
+//     System.out.println(j);
+//     System.out.println(arr[2]);
+//     System.out.println(343432/0);
+// } catch (ArithmeticException e) {
+// System.out.println("something went wrong ");
+// }
+// catch(ArrayIndexOutOfBoundsException e){
+//     System.out.println("Array index is out of bounds");
+// }
+// catch(Exception e){
+//     System.out.println("Something went wrong");     
+// }
+
+// System.out.println(a*b);
+
+
+//     }
+// }
+// // object -> throwable 
+// // error // expections
+
+
+// throws exception
+// 1 compile time error
+// 2 Run time error - exception
+// 3 logical error
+
+// int a=34342;
+// int b=10;
+// int i=0;
+// int arr[]=new int[3];
+// arr[2]=23;
+
+// try {
+//     int j=343432/2;
+//     System.out.println(j);
+//     System.out.println(arr[2]);
+//     System.out.println(343432/0);
+// } catch (ArithmeticException e) {
+// System.out.println("something went wrong ");
+// }
+// catch(ArrayIndexOutOfBoundsException e){
+//     System.out.println("Array index is out of bounds");
+// }
+// catch(Exception e){
+//     System.out.println("Something went wrong");
+// }
+
+// System.out.println(a*b);
+
+
+//     }
+// }
+// // object -> throwable
+// // error // exceptions   
